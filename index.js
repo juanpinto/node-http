e0c057a594521f36d3d100d181f2c12ed8e2f402
Juan Pinto <jpinto@grupov.com.ve>
2020-03-27 20:56:06 -0400

    Node Http. First Exercise

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http')
+
+const hostname = 'localhost'
+const port = 3000
+
+
+const server = http.createServer((request, response) => {
+    console.log(request.headers)
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+    response.end('<html><body><h1>Hello, World</h1></body></html>')
+})
+
+
+server.listen(port, hostname, () => {
+    console.log(`Server is running at http//:${hostname}:${port}`)
+})
