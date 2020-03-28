7afd4e930899cfde218d199eee6fa5d790a3c4bb
Juan Pinto <jpinto@grupov.com.ve>
2020-03-27 22:08:41 -0400

    Simple Http Excercise

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
-const http = require('http')
+const http = require('http');
+const fileSistem = require('fs');
+const path = require('path');
 
 const hostname = 'localhost'
 const port = 3000
 
 
 const server = http.createServer((request, response) => {
-    console.log(request.headers)
+    if(request.method == 'GET') {
+        var fileUrl
+        console.log(request.url)
+        if(request.url == '/') {
+            fileUrl = 'index.html'
+        } else {
+            fileUrl = request.url
+        }
+        var filePath = path.resolve(`./public/${fileUrl}`)
+        const fileExtension = path.extname(filePath)
+        if(fileExtension == '.html') {
+            fileSistem.exists(filePath, (exist) => {
+                if(!exist) {
+                    response.statusCode = 404;
+                    response.setHeader('Content-Type', 'text/html');
+                    response.end(`<html><body><h1>Error 404: file ${fileUrl} Not found</h1></body></html>`);
+                    return;
+                }
+                response.statusCode = 200;
+                response.setHeader('Content-Type', 'text/html');
+                fileSistem.createReadStream(filePath).pipe(response);
+            })
+        } else {
+            response.statusCode = 404;
+            response.setHeader('Content-Type', 'text/html');
+            response.end(`<html><body><h1>Error 404: file ${fileUrl} is not an html file</h1></body></html>`);
+            return;
+        }
 
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/html');
-    response.end('<html><body><h1>Hello, World</h1></body></html>')
+    } else {
+        response.statusCode = 404;
+        response.setHeader('Content-Type', 'text/html');
+        response.end(`<html><body><h1>Error 404: Request Method: ${request.method} not supported</h1></body></html>`);
+        return;
+    }
 })
 
 
